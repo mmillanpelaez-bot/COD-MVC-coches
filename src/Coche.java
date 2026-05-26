01b475e0ccb4a1cb9bd250dfc533a0d628ab8dd3
Felipe <felipe@example.com>
2026-05-26 11:42:02 +0000

    añado campo gasolina a Coche

--- src/Coche.java
+++ src/Coche.java
@@ -2,10 +2,12 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
+    double gasolina;
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
+        this.gasolina = 0.0;
     }
 }
